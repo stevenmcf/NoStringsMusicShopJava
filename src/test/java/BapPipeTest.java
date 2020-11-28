ccd830d27f264645d9419466949f73c08989db0e
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-28 13:32:20 +0000

    basic getters & setters tested quickly for bagpipe. ENUM not yet tested

--- src/test/java/BapPipeTest.java
+++ src/test/java/BapPipeTest.java
@@ -1,2 +1,27 @@
+import instruments.BagPipeType;
+import instruments.BagPipes;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BapPipeTest {
+
+    public BagPipes myChanters;
+
+    @Before
+    public void setUp(){
+        myChanters = new BagPipes("Duncan MacRae", "SL1", 950, 1150);
+    }
+
+    @Test
+    public void canGetRetailPrice(){
+        assertEquals(1150, myChanters.getRetailPrice(), 0.01);
+    }
+
+    @Test
+    public void canSetNewRetailPrice(){
+        myChanters.setRetailPrice(850);
+        assertEquals(850, myChanters.getRetailPrice(), 0.01);
+    }
 }
